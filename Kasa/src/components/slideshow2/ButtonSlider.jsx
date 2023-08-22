c65b303249652b7bc55103c41d3920fd747aaa46
EvivNotrub <barthwerle@yahoo.de>
2023-08-22 10:32:59 +0200

    timeout on picture change

--- Kasa/src/components/slideshow2/ButtonSlider.jsx
+++ Kasa/src/components/slideshow2/ButtonSlider.jsx
@@ -1,19 +1,17 @@
 import './buttonSlider.scss';
 
-// function ButtonSlider({ handleNext, handlePrevious }) {
-//     return (
-//         <div className='buttonSlider'>
-//             <button onClick={handlePrevious} className='buttonSlider__arrow --left'></button>
-//             <button onClick={handleNext} className='buttonSlider__arrow --right'></button>
-//         </div>
-//     )
-// }
 
 function ButtonSlider({ next, previous }) {
     return (
         <div className='buttonSlider'>
-            <button onClick={previous} className='buttonSlider__arrow --left'></button>
-            <button onClick={next} className='buttonSlider__arrow --right'></button>
+            <button type='button' 
+                onClick={ () => { setTimeout(() => { previous() }, 1500)}}
+                className='buttonSlider__arrow --left'>
+            </button>
+            <button type='button'
+                onClick={() => { setTimeout(() => { next() }, 1500)}}
+                className='buttonSlider__arrow --right'>
+            </button>
         </div>
 
     )
