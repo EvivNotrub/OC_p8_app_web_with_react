d3259ce0977a6bc734fdd6ede9a5246cf5b82d5f
EvivNotrub <barthwerle@yahoo.de>
2023-09-08 18:02:52 +0200

    banner filter className added

--- Kasa/src/components/banner/Banner.jsx
+++ Kasa/src/components/banner/Banner.jsx
@@ -3,7 +3,7 @@ import './banner.scss'
 function Banner({ image, title }) {
     return (
         <h1 className='banner'>
-            <img className='banner__img' src={image} alt="photo d'un paysage" />
+            <img className={'banner__img' + (title ? ' title-dim' : '')} src={image} alt="photo d'un paysage" />
             {title && <span className='banner__title'>{title}</span>}
         </h1>
     )
