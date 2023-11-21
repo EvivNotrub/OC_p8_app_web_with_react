9bac7554ab3f7936a28bf7427040a77be2aefc25
EvivNotrub <barthwerle@yahoo.de>
2023-11-22 00:41:31 +0100

    different cases for serve and run dev base path

--- Kasa/vite.config.js
+++ Kasa/vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/OC_p8_app_web_with_react/",
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+    server: {
+      port: 3000,
+    },
+  }
+
+  if (command !== 'serve') {
+    config.base = '/OC_p8_app_web_with_react/'
+  }
+
+  return config
 })
