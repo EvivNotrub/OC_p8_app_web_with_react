b623927ba2b9e978e7d40fb2259c58c2a599384c
EvivNotrub <barthwerle@yahoo.de>
2023-11-21 23:10:20 +0100

    config vite base for deployment

--- Kasa/vite.config.js
+++ Kasa/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/OC_p8_app_web_with_react/',
   plugins: [react()],
   server: {
     port: 3000,
